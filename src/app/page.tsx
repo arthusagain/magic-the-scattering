5e1da100cdab0fce4eb6c03678120347ca2c544a
arthusagain <arthus.james@hotmail.com>
2025-02-09 08:35:49 -0300

    Better routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,24 +12,28 @@ import { EditProfile } from "../pages/EditProfile";
 import { CardInfo } from "../pages/CardInfo";
 import { CardCreate } from "../pages/CardCreate";
 import { NoPage } from "../pages/NoPage";
+import Banner from '@/components/Banner';
 
 
 export default function Home() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />} >
-          <Route index element={<Homepage />} />
-          <Route path="auth" element={<Auth />} />
-          <Route path="about" element={<About />} />
-          <Route path="search" element={<Search />} />
-          <Route path="profile" element={<Profile />} />
-          <Route path="edit-profile" element={<EditProfile />} />
-          <Route path="card-info" element={<CardInfo />} />
-          <Route path="card-create" element={<CardCreate />} />
-          <Route path="*" element={<NoPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Banner />
+        <Routes>
+          <Route path="/" element={<Layout />} >
+            <Route index element={<Homepage />} />
+            <Route path="auth" element={<Auth />} />
+            <Route path="about" element={<About />} />
+            <Route path="search" element={<Search />} />
+            <Route path="profile" element={<Profile />} />
+            <Route path="edit-profile" element={<EditProfile />} />
+            <Route path="card-info" element={<CardInfo />} />
+            <Route path="card-create" element={<CardCreate />} />
+            <Route path="*" element={<NoPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,22 +1,15 @@
-import { Outlet} from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Banner from ".././components/Banner";
 import React from "react";
 
-interface Props{
+interface Props {
     children?: React.ReactNode
 }
 
-export const Layout = ({children}: Props) => {
+export const Layout = ({ children }: Props) => {
     return (
-        <>
-            <div>
-                <header>
-                    <Banner />
-                </header>
-                <main className="main-container">
-                    <Outlet />
-                </main>
-            </div>
-        </>
+        <div className="main-container">
+            <Outlet />
+        </div>
     )
 };
