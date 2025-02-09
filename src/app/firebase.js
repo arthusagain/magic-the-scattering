5870c039e27a4e74193734632ac11860ba357392
arthusagain <arthus.james@hotmail.com>
2025-02-09 20:42:58 -0300

    firebase initial config

---
+++ src/app/firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCoMG3I_vVVjkxpVpI6ijbxbymVxIwarxk",
+  authDomain: "magic-the-scattering.firebaseapp.com",
+  projectId: "magic-the-scattering",
+  storageBucket: "magic-the-scattering.firebasestorage.app",
+  messagingSenderId: "627130647103",
+  appId: "1:627130647103:web:441cecacc7e68d8f476e8a",
+  measurementId: "G-5ZVD847MQ9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const analytics = getAnalytics(app);
+export {db}
